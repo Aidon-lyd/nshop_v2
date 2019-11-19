e90ed1682d60fd8d0dfe0fd77998999829f7c083
DELL <2460694748@qq.com>
2019-11-19 19:38:59 +0800

    day11-19

---
+++ ods/sqoop/full_import_ods.sh
@@ -0,0 +1,22 @@
+--用户基本信息表
+sqoop import "-Dorg.apache.sqoop.splitter.allow_text_splitter=true" \
+--connect jdbc:mysql://node245:3306/nshop \
+--username root \
+--password 12345678 \
+--table customer \
+--hive-import \
+--hive-overwrite \
+--hive-table 1902_wzh_ods_nshop.ods_nshop_02_customer \
+-m 10
+
+
+--收货地址管理表
+sqoop import "-Dorg.apache.sqoop.splitter.allow_text_splitter=true" \
+--connect jdbc:mysql://node245:3306/nshop \
+--username root \
+--password 12345678 \
+--table customer_consignee \
+--hive-import \
+--hive-overwrite \
+--hive-table 1902_wzh_ods_nshop.ods_nshop_02_customer_consignee \
+-m 10
