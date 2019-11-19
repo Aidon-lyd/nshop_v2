212d46ef2db8a8b6bf33045b1776c14dd89ab71b
tianzhonggen <805011313@qq.com>
2019-11-19 17:07:42 +0800

    dim和ods层建表语句及全量,增量导入语句

---
+++ dim/import_dim_full.sh
@@ -0,0 +1,69 @@
+sqoop import \
+--connect jdbc:mysql://node245:3306/nshop \
+--username root \
+--password 12345678 \
+--table category \
+--hive-import \
+--hive-overwrite \
+--hive-table 1902_tzg_dim_nshop.dim_pub_category \
+-m 1
+
+sqoop import  \
+--connect jdbc:mysql://node245:3306/nshop \
+--username root \
+--password 12345678 \
+--table supplier \
+--hive-import \
+--hive-overwrite \
+--hive-table 1902_tzg_dim_nshop.dim_pub_supplier \
+-m 1
+
+sqoop import "-Dorg.apache.sqoop.splitter.allow_text_splitter=true" \
+--connect jdbc:mysql://node245:3306/nshop \
+--username root \
+--password 12345678 \
+--table product \
+--hive-import \
+--hive-overwrite \
+--hive-table 1902_tzg_dim_nshop.dim_pub_product \
+-m 10
+
+sqoop import "-Dorg.apache.sqoop.splitter.allow_text_splitter=true" \
+--connect jdbc:mysql://node245:3306/nshop \
+--username root \
+--password 12345678 \
+--table page_dim \
+--hive-import \
+--hive-overwrite \
+--hive-table 1902_tzg_dim_nshop.dim_pub_page \
+-m 10
+
+sqoop import \
+--connect jdbc:mysql://node245:3306/nshop \
+--username root \
+--password 12345678 \
+--table area_dim \
+--hive-import \
+--hive-overwrite \
+--hive-table 1902_tzg_dim_nshop.dim_pub_area \
+-m 1
+
+sqoop import \
+--connect jdbc:mysql://node245:3306/nshop \
+--username root \
+--password 12345678 \
+--table date_dim \
+--hive-import \
+--hive-overwrite \
+--hive-table 1902_tzg_dim_nshop.dim_pub_date \
+-m 1
+
+sqoop import \
+--connect jdbc:mysql://node245:3306/nshop \
+--username root \
+--password 12345678 \
+--table comm_dim \
+--hive-import \
+--hive-overwrite \
+--hive-table 1902_tzg_dim_nshop.dim_pub_comm \
+-m 10
---
+++ ods/ods_create_table.sql
@@ -0,0 +1,83 @@
+create table if not exists 1902_tzg_ods_nshop.ods_nshop_02_customer (
+ customer_id string COMMENT '用户ID',
+ customer_login string COMMENT '用户登录名',
+ customer_nickname string COMMENT '用户名(昵称)',
+ customer_name string COMMENT '用户真实姓名',
+ customer_pass string COMMENT '用户密码',
+ customer_mobile string COMMENT '用户手机',
+ customer_idcard string COMMENT '身份证',
+ customer_gender TINYINT COMMENT '性别：1男 0女',
+ customer_birthday string COMMENT '出生年月',
+ customer_age TINYINT COMMENT '性别：1男 0女',
+ customer_age_range string COMMENT '年龄段(01->[0,17],02->[18-45],03->[46-60],04->[60-100])',
+ customer_email string COMMENT '用户邮箱',
+ customer_natives string COMMENT '所在地区',
+ customer_ctime bigint COMMENT '创建时间',
+ customer_utime bigint COMMENT '修改时间',
+ customer_device_num string COMMENT '用户手机设备号'
+)
+location '/data/tzg/ods/ods_nshop_02_customer/';
+
+create table if not exists 1902_tzg_ods_nshop.ods_nshop_02_customer_consignee (
+ consignee_id string COMMENT '收货地址ID',
+ customer_id string COMMENT '用户ID',
+ consignee_name string COMMENT '收货人',
+ consignee_mobile string COMMENT '收货人电话',
+ consignee_zipcode string COMMENT '收货人地区',
+ consignee_addr string COMMENT '收货人详细地址',
+ consignee_tag string COMMENT '标签：1家 2公司 3学校',
+ ctime bigint COMMENT '创建时间'
+)
+location '/data/tzg/ods/ods_nshop_02_customer_consignee/';
+
+create table if not exists 1902_tzg_ods_nshop.ods_nshop_02_orders (
+ order_id string COMMENT '订单ID(时间+商品ID+4位随机)',
+ customer_id string COMMENT '下单用户ID',
+ order_status TINYINT COMMENT '订单状态',
+ customer_ip string COMMENT '下单用户IP',
+ user_longitude string COMMENT '用户地理：经度',
+ user_latitude string COMMENT '用户地理：纬度',
+ user_areacode string COMMENT '用户所在地区',
+ consignee_name string COMMENT '收货人',
+ consignee_mobile string COMMENT '收货人电话',
+ consignee_zipcode string COMMENT '收货人地址',
+ pay_type string COMMENT '支付类型：线上支付 10 网上银行 11 微信 12 支付宝 | 线下支付(货到付款) 20 ',
+ pay_code string COMMENT '支付对应唯一标识，如微信号、支付宝号',
+ pay_nettype string COMMENT '支付网络方式：0 wifi | 1 4g | 2 3g |3 线下支付',
+ district_money DECIMAL(8,1) COMMENT '优惠金额',
+ shipping_money DECIMAL(8,1)  COMMENT '运费金额',
+ payment_money DECIMAL(10,1) COMMENT '支付金额',
+ order_ctime bigint  COMMENT '创建时间',
+ shipping_time bigint  COMMENT '发货时间',
+ receive_time bigint  COMMENT '收货时间'
+) partitioned by (bdp_day string)
+location '/data/tzg/ods/ods_nshop_02_orders/';
+
+create table if not exists 1902_tzg_ods_nshop.ods_nshop_02_order_detail(
+ order_detail_id string COMMENT '订单详情表ID',
+ order_id string COMMENT '订单表ID',
+ product_id string COMMENT '订单商品ID',
+ product_name string COMMENT '商品名称',
+ product_remark string COMMENT '商品描述',
+ product_cnt INT  COMMENT '购买商品数量',
+ product_price DECIMAL(5,1)  COMMENT '购买商品单价',
+ weighing_cost DECIMAL(2,1)  COMMENT '商品加权价格',
+ district_money DECIMAL(4,1)  COMMENT '优惠金额',
+ is_activity bigint COMMENT '1:参加活动|0：没有参加活动',
+ order_detail_ctime bigint COMMENT '下单时间'
+)partitioned by (bdp_day string)
+location '/data/tzg/ods/ods_nshop_02_order_detail/';
+
+create table if not exists 1902_tzg_ods_nshop.ods_nshop_02_orders_pay_records (
+ pay_id string COMMENT '支付记录ID',
+ order_id string COMMENT '订单ID',
+ customer_id string COMMENT '用户ID',
+ pay_status string COMMENT '支付状态：0 支付失败| 1 支付成功',
+ pay_type string COMMENT '支付类型：线上支付 10 网上银行 11 微信 12 支付宝 | 线下支付(货到付款) 20 ',
+ pay_code string COMMENT '支付对应唯一标识，如微信号、支付宝号',
+ pay_nettype string COMMENT '支付网络方式：1 wifi | 2 4g | 3 3g |4 线下支付',
+ pay_amount double COMMENT '支付金额',
+ pay_ctime bigint  COMMENT '创建时间'
+)partitioned by (bdp_day string)
+location '/data/tzg/ods/ods_nshop_02_orders_pay_records/';
+
